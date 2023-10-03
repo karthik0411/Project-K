8c9615b6a0a7cfd5a44900975cddbd2b53160cff
saicharan0312 <saicharan0312@gmail.com>
2023-10-02 21:28:31 -0400

    just project has been done added the directory

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Profile from './pages/UserPages/Profile';
+
+function App() {
+  return (
+    <div className="App">
+      <Profile />
+    </div>
+  );
+}
+
+export default App;
